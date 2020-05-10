14536fec803c4d00cd11aa25d6d5dd878ef4640d
L5047 <l5047@student.jamk.fi>
2020-05-10 18:34:53 +0300

    Remove relative paths

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       key: "./server.key",
       ref: "origin/master",
       repo: "https://github.com/TSkoll/rei-ts.git",
-      path: "~/rei-ts",
+      path: "rei-ts",
       "post-deploy": "npm run build && pm2 reload ecosystem.config.js --env production",
     },
   },
