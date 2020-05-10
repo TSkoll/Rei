fe358e41e77b8c0c1a0cd55d85cd6b4f39c42ae0
L5047 <l5047@student.jamk.fi>
2020-05-10 23:47:16 +0300

    Implement send

---
+++ src/modules/owner/send.ts
@@ -0,0 +1,20 @@
+import Command from "../../types/Command";
+import { CommandMessage } from "../../extensions/Message";
+import { TextChannel } from "discord.js";
+
+export default class Send extends Command {
+  constructor() {
+    super({
+      ownerOnly: true,
+    });
+  }
+
+  public async run(message: CommandMessage, args: string[]) {
+    const guild = args[0];
+    const channel = args[1];
+    const sent = args[2];
+
+    const textChannel = message.client.guilds.cache.get(guild)!.channels.cache.get(channel)! as TextChannel;
+    textChannel.send(sent);
+  }
+}
