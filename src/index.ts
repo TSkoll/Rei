46fdc80c0f2d73f41ce8d2a16bb6e7f70afa8b8b
L5047 <l5047@student.jamk.fi>
2020-05-10 22:27:55 +0300

    Add explanation for root try catch

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,7 @@ mongoose.connect(mongooseConn, mongooseConnOpt, async err => {
 
     client.login(config.token);
   } catch (err) {
+    // Leads into cleaner error output on a catastrophic error
     console.error(err);
     process.exit(1);
   }
