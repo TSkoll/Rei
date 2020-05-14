509ea0a880a76cf00b39b963f6b12302d8e862a0
L5047 <l5047@student.jamk.fi>
2020-05-14 20:51:02 +0300

    Have prod server output node and npm versions before setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,6 +15,7 @@ module.exports = {
       ref: "origin/master",
       repo: "https://github.com/TSkoll/rei-ts.git",
       path: "/home/ubuntu/rei-ts",
+      "pre-setup": "node -v && npm -v",
       "post-deploy":
         "mkdir -p ./data && cp /home/ubuntu/config.json ./data/config.json && npm run clean && npm run build && pm2 reload ecosystem.config.js --env production",
     },
