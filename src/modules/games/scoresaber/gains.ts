84688ce2f1e5747a74a9896c6a2a11ecdc3dadcb
MakeMonni <46992293+MakeMonni@users.noreply.github.com>
2021-02-23 15:34:42 +0200

    Update gains.ts
    Added a new alias

--- src/modules/games/scoresaber/gains.ts
+++ src/modules/games/scoresaber/gains.ts
@@ -12,7 +12,7 @@ export default class Gains extends SubCommand {
   constructor(parent: Command | SubCommand) {
     super(parent, {
       options: {
-        aliases: ["losses"],
+        aliases: ["losses"], ["sustain"],
       },
       help: {
         description:
