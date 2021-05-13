0426e5a5a4959e729f233333f50981d4cc5115dc
Eero Ronkainen <ronkainen.eero@gmail.com>
2021-05-13 16:46:37 +0300

    Fetch members on starboard init

--- src/modules/owner/starboard.ts
+++ src/modules/owner/starboard.ts
@@ -6,6 +6,7 @@ import { TextChannel } from "discord.js";
 
 export default class Starboard extends Command {
   private starEmoji: string = "%E2%AD%90";
+  private starboardGuild: string = "724661988768350239";
   private lookupChannel: string = "724715692804407346";
   private postChannel: string = "728915959024320513";
 
@@ -23,6 +24,11 @@ export default class Starboard extends Command {
   }
 
   public async afterInit(client: ReiClient) {
+    // The "messageReactionAdd" event is only triggered
+    // on cached message for cached guild members
+    const guild = await client.guilds.fetch(this.starboardGuild, true);
+    await guild.members.fetch();
+
     /*
       This is a hacky solution to add reaction handling support to a command
       if a new need for reactions appear, this functionality should be generalized
